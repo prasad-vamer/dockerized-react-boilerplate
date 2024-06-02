b51a9e1725f0f60844e6f13e682d13cbf8817405
prasad-vamer <prasadkrishnakptp@gmail.com>
2024-06-02 23:59:28 +0900

    feat: Integrate react-redux for state management
    Add react-redux library and configure the store to manage the example state. Update the Home component to use the example state value and dispatch increment and decrement actions.

--- src/Container/Home.tsx
+++ src/Container/Home.tsx
@@ -1,7 +1,13 @@
 import { Button } from "@/shadcn/components/ui/button";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { decrement, increment } from "../store//exampleSlice";
 
 function Home() {
+  const value = useSelector((state: RootState) => state.example.value)
+  const dispatch = useDispatch()
+
   return (
     <div className="bg-purple-400">
       <h1>Home Page</h1>
@@ -10,7 +16,13 @@ function Home() {
       <br />
       <Link to={"/about"}>Go to About Page</Link>
       <br />
-      <Button>Shadcn Button</Button>
+      example state's value: {value}
+      <br />
+      <div className="flex gap-2">
+        <Button>Shadcn Button</Button>
+        <Button onClick={():void => {dispatch(increment())}}>Increase Count</Button>
+        <Button onClick={():void => {dispatch(decrement())}}>Decrease count</Button>
+      </div>
     </div>
   )
 }
